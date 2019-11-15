67c2cd3e6d60c30493f7c522583c5bd42c7ca40f
dudeperf3ct <shubham.gandhi7@gmail.com>
2019-11-15 16:17:58 +0000

    complete hw0

--- src/process_image.c
+++ src/process_image.c
@@ -7,18 +7,30 @@
 float get_pixel(image im, int x, int y, int c)
 {
     // TODO Fill this in
-    return 0;
+    if (x >= 0 && x < im.w && y >= 0 && y < im.h && c >= 0 && c < im.c)
+    {
+        return im.data[x + (y * im.w) + (c * im.w * im.h)];
+    }
+    else{
+        clamp_image(im);
+        return im.data[x + (y * im.w) + (c * im.w * im.h)];
+    }
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
     // TODO Fill this in
+    if (x >= 0 && x < im.w && y >= 0 && y < im.h && c >= 0 && c < im.c)
+    {
+        im.data[x + (y * im.w) + (c * im.w * im.h)] = v;
+    }
 }
 
 image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
     // TODO Fill this in
+    memcpy(copy.data, im.data, sizeof(im));
     return copy;
 }
 
@@ -27,17 +39,45 @@ image rgb_to_grayscale(image im)
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
     // TODO Fill this in
+    float c0=0., c1=0., c2=0.;
+    for (int i=0; i<im.h; i++){
+        for (int j=0; j<im.w; j++){
+            c0 = get_pixel(im, j, i, 0);
+            c1 = get_pixel(im, j, i, 1);
+            c2 = get_pixel(im, j, i, 2);
+            gray.data[i + j] = 0.299*c0 + 0.587*c1 + 0.114*c2;
+        }
+    }
     return gray;
 }
 
 void shift_image(image im, int c, float v)
 {
     // TODO Fill this in
+    for (int i=0; i<im.h; i++){
+        for (int j=0; j<im.w; j++){
+            set_pixel(im, j, i, c, get_pixel(im, j, i, c) + v);
+        }
+    }
 }
 
 void clamp_image(image im)
 {
     // TODO Fill this in
+    for (int k=0; k<im.c; k++){
+        for (int i=0; i<im.h; i++){
+            for (int j=0; j<im.w; j++){
+                if (get_pixel(im, j, i, k) < 0)
+                {
+                    set_pixel(im, j, i, k, 0);
+                }
+                if (get_pixel(im, j, i, k) > 1)
+                {
+                    set_pixel(im, j, i, k, 1);
+                }                
+            }
+        }
+    }
 }
 
 
